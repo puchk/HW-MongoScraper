bc42d7e4ca6fa653845e1b7898e0bfe90cc8d087
Rutul Patel <rutul-patel@live.com>
2017-06-07 20:51:48 -0700

    Update models

--- models/Article.js
+++ models/Article.js
@@ -18,10 +18,6 @@ var ArticleSchema = new Schema({
     link: {
         type: String,
         required: true
-    },
-    note: {
-        type: Schema.Types.ObjectId,
-        ref: "Note"
     }
 });
 
--- models/Note.js
+++ models/Note.js
@@ -3,14 +3,18 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var NoteSchema = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    body: {
-        type: String,
-        required: true
-    }
+  title: {
+    type: String,
+    required: true
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  articleId: {
+    type: Schema.Types.ObjectId,
+    ref: "Article"
+  }
 });
 
 var Note = mongoose.model("Note", NoteSchema);
